8b40474f3a32e49f2f0887414fd9b6b5ab6babb2
MarinaGalinova <m.kriklivenko@gmail.com>
2023-05-24 20:42:37 +0300

    upd

--- src/test/java/CardFormTest.java
+++ src/test/java/CardFormTest.java
@@ -10,16 +10,16 @@ public class CardFormTest {
 
     private WebDriver driver;//поле под вебдрайвер с переменной driver, которую мы будем использовать для обращения к драйверу
 
-    @BeforeAll //устанавливаем драйвер хрома, прописываем путь к нему в этом проекте
-    static void SetUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-    }
+//    @BeforeAll //устанавливаем драйвер хрома, прописываем путь к нему в этом проекте
+//    static void SetUpAll() {
+//        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+//    }
 
-    @BeforeEach
-        //перед каждым тестом открываем новую версию хрома, чтобы не переходили какие-то ранее запущенные действия
-    void setUp() {
-        driver = new ChromeDriver();
-    }
+//    @BeforeEach
+//        //перед каждым тестом открываем новую версию хрома, чтобы не переходили какие-то ранее запущенные действия
+//    void setUp() {
+//        driver = new ChromeDriver();
+//    }
 
     @BeforeEach
     void setUpChrome() {
